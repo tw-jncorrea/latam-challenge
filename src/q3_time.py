1868faf2f10c3b2c6118e6e4d05dbb17300d2eed
tw-jncorrea <jessica.correa@thoughtworks.com>
2024-04-06 22:13:03 -0500

    feat: Add functions for Q03
    Add processing BigQuery functions

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,15 @@
-from typing import List, Tuple
+import datetime 
+from typing import List, Tuple  
+from google.cloud import bigquery  
+from process import process_bigquery_results  
 
-def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+def q3_time(client: bigquery.Client) -> List[Tuple[datetime.date, str]]:
+    query = """
+      SELECT user.username, count(*) AS conteo_menciones
+      FROM tweets_dataset.tweets,
+      UNNEST(mentionedUsers) AS user
+      group by user.username
+      order by conteo_menciones desc
+      LIMIT 10
+      """
+    return process_bigquery_results(client, query)
